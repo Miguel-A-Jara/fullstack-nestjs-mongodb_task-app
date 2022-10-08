e76f7065b3518ea774e3139bc08d59d8232a8eaa
Miguel-A-Jara <0miguel.jara15@gmail.com>
2022-10-07 22:06:00 -0300

    docker-compose con mongo finalizado

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { TodosModule } from './todos/todos.module';
 
-console.log("\n\n\n\n\n MONGODB:",process.env.MONGODB,"\n\n\n\n\n\n")
+console.log("\n\n\n\n\n MONGODB:",process.env.MONGO_URL,"\n\n\n\n\n\n")
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRoot(process.env.MONGODB),
+    MongooseModule.forRoot(process.env.MONGO_URL),
     TodosModule,
   ],
   controllers: [],
