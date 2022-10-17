f19bd1689e4bfde22293789e866e60c22441721b
Miguel Jara <miguel.jara@clt.com.py>
2022-10-17 14:20:26 -0300

    Hashing the passwords and returning jwt token

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,14 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable }  from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model }       from 'mongoose';
+import * as bcrypt from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
+
 import { CreateUserDto } from './dto/create-user.dto';
-import { LoginUserDto } from './dto/login-user.dto';
+import { LoginUserDto }  from './dto/login-user.dto';
+import { User, UserDocument } from './schema/users.schema';
 
 @Injectable()
 export class UsersService {
-  signUpUser(createUserDto: CreateUserDto) {
-    return createUserDto;
+
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>){}
+
+  private createToken(id: string) {
+    return jwt.sign({id}, process.env.SECRET, { expiresIn: '3d' });
   }
 
-  logInUser(loginUserDto: LoginUserDto) {
-    return loginUserDto;
+  async signUpUser(createUserDto: CreateUserDto) {
+
+    const { username, email, password } = createUserDto;
+
+    const user = await this.userModel.findOne({ $or: [ { username }, { email } ] });
+
+    if ( user )
+      throw new BadRequestException('User already exists');
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);    
+
+    const createdUser = await this.userModel.create({ ...createUserDto, password: hash });
+    const token = this.createToken(createdUser.id);
+
+    return { createdUser, token };
+  }
+
+  async logInUser(loginUserDto: LoginUserDto) {
+
+    const { email, password } = loginUserDto;
+
+    const user = await this.userModel.findOne({ $and: [ { email }, { password } ] });
+
+    if (!user) 
+      throw new BadRequestException('Account not found');
+
+    const token = this.createToken(user._id.toString());
+    
+    return { user, token };
   }
 }
